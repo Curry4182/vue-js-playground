0d59031431493ecb84226131139dacc56ee548a8
Curry4182 <kangbk4182@naver.com>
2024-08-14 14:05:06 +0900

    feat: axios를 이용하여 로그인 예제 추가

---
+++ vue3-form/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <form @submit.prevent="submitLogin">
+      <div>
+        <label for="userId">ID:</label>
+        <input id="userId" type="text" v-model="userId">
+      </div>
+      <div>
+        <label for="password">PW:</label>
+        <input id="password" type="text" v-model="password">
+      </div>
+      <button type="submit">로그인</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        userId: '',
+        password: '',
+      }
+    },
+    methods: {
+      submitLogin() {
+        const data = {
+          'userId': this.userId,
+          'password': this.password
+        };
+
+        axios.post('https://jsonplaceholder.typicode.com/users', data)
+          .then(res => {
+            console.log(res)
+          });
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
